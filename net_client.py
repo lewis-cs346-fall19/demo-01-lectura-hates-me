3476d19e637fef2ad3219746c9bc4fcb54c825bf
Chris Wang <chriswang19@gmail.com>
2019-09-03 10:36:45 -0700

    fixed try block

--- net_client.py
+++ net_client.py
@@ -4,8 +4,8 @@ addr = ("localhost", 17000)
 sock.connect(addr)
 for x in range(0,100):
     msg = "hello " + str(x)
-    sock.sendall(msg.encode())
     try:
+        sock.sendall(msg.encode())
         ans = sock.recv(1024).decode()
         print("The reply is: " + ans)
     except:
