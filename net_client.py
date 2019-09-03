5095061193ec2882668fd9900a29cd5f42f5d6c7
Chris Wang <chriswang19@gmail.com>
2019-09-03 10:46:21 -0700

    added print statements

--- net_client.py
+++ net_client.py
@@ -2,12 +2,16 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 addr = ("localhost", 17000)
 sock.connect(addr)
+print("connected")
 for x in range(0,100):
+    print("in for loop")
     msg = "hello " + str(x)
     try:
+        print("client try")
         sock.sendall(msg.encode())
         ans = sock.recv(1024).decode()
         print("The reply is: " + ans)
     except:
+        print("client closed")
         sock.close()
 
