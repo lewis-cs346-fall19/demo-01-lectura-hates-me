bc5278246b0713c40de4d8dbc017cd7e3179ad41
Chris Wang <chriswang19@gmail.com>
2019-09-03 09:59:47 -0700

    fixed typeerror

--- net_client.py
+++ net_client.py
@@ -3,7 +3,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 addr = ("localhost", 17000)
 sock.connect(addr)
 for x in range(0,100):
-    msg = "hello " + x
+    msg = "hello " + str(x)
     sock.sendall(msg.encode())
     try:
         ans = sock.recv(1024).decode()
