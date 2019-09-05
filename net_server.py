89191749ef880f16919873e01f89210bea627724
GrantJMay <47539768+GrantJMay@users.noreply.github.com>
2019-09-05 11:48:02 -0700

    Add files via upload
    removed de-bugging print statements

--- net_server.py
+++ net_server.py
@@ -4,14 +4,10 @@ addr = ("0.0.0.0", 17000)
 sock.bind(addr)
 sock.listen(5)
 while True:
-    print("here")
     (connectedSock, clientAddress)=sock.accept()
     try:
-        print("try")
         msg=connectedSock.recv(1024).decode()
         print("Here's the data: " +msg)
         connectedSock.sendall(msg.encode())
     except ConnectionAbortedError:
-        print('closed')
         connectedSock.close()
-    print('looped')
