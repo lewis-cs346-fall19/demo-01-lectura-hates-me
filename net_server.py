29dbe496b2b2033af99f3350a05671d8cf3562cd
GrantJMay <47539768+GrantJMay@users.noreply.github.com>
2019-09-05 19:02:12 -0700

    Add files via upload

--- net_server.py
+++ net_server.py
@@ -3,9 +3,9 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 addr = ("0.0.0.0", 17000)
 sock.bind(addr)
 sock.listen(5)
+(connectedSock, clientAddress)=sock.accept()
 while True:
     try:
-        (connectedSock, clientAddress)=sock.accept()
         msg=connectedSock.recv(1024).decode()
         print("Here's the data: " +msg)
         connectedSock.sendall(msg.encode())
