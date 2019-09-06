e5ad48c1fcc69ac005f4e06da9532ee61492ba01
GrantJMay <47539768+GrantJMay@users.noreply.github.com>
2019-09-06 13:20:46 -0700

    Add files via upload
    added sys.exit(0)

--- net_server.py
+++ net_server.py
@@ -1,3 +1,4 @@
+import sys
 import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 addr = ("0.0.0.0", 17000)
@@ -9,5 +10,6 @@ while True:
         msg=connectedSock.recv(1024).decode()
         print("Here's the data: " +msg)
         connectedSock.sendall(msg.encode())
-    except:
+    except ConnectionAbortedError:
         connectedSock.close()
+        sys.exit(0)
